7f7271f9691d7bdb4114c9def26a4e87c6090543
Dipesh Rajoria <dipeshrajoria45@gmail.com>
2023-07-12 12:05:37 +0530

    my solution

---
+++ purgecss.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  content: ["./**/*.html", "./**/*.js"], // Files to analyze for used CSS classes and selectors
+  css: ["./**/*.css"], // CSS files to analyze
+  // Other configuration options
+};
